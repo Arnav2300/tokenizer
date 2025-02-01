6e12e24fcc2e16ecf8f89af119333164334558a5
Arnav2300 <56104491+Arnav2300@users.noreply.github.com>
2025-02-01 22:04:58 +0000

    :art: Format Python code with psf/black

--- custom_types/corpus.py
+++ custom_types/corpus.py
@@ -2,31 +2,32 @@ from typing import List, Tuple
 
 
 class Corpus:
-    def __init__(self,data:List[Tuple[str,int]]):
+    def __init__(self, data: List[Tuple[str, int]]):
         """
-            Initializes the Corpus with a list of word-freqeuncy tuples.
+        Initializes the Corpus with a list of word-freqeuncy tuples.
         """
-        self.data=data
+        self.data = data
 
-    def add(self,word:str,frequency:int):
+    def add(self, word: str, frequency: int):
         """
-            Adds a new word-frequency pair to the Corpus.
+        Adds a new word-frequency pair to the Corpus.
         """
-        self.data.append((word,frequency))
+        self.data.append((word, frequency))
 
-    def get(self,word:str)->int:
+    def get(self, word: str) -> int:
         """
-            Gets the frequency of a word in the corpus.
+        Gets the frequency of a word in the corpus.
         """
-        for w,freq in self.data:
-            if w==word:
+        for w, freq in self.data:
+            if w == word:
                 return freq
         return 0  # Return 0 if the word is not found
+
     def __repr__(self):
         return f"Corpus({self.data})"
-    
+
     def to_dict(self):
         """
-            Converts Corpus to a dictionary.
+        Converts Corpus to a dictionary.
         """
         return dict(self.data)
